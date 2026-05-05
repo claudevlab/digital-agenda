9d840d83ce30a48b054b26fed6a4c8d62b2ef81c
claudevlab <claudionugnes@gmail.com>
2026-05-05 05:43:08 +0200

    effetuate delle modifiche CORS per renderlo compatibile con il dominio in produzione

--- backend/digital-agenda-private/src/main/java/com/claudev/agenda/config/CorsConfig.java
+++ backend/digital-agenda-private/src/main/java/com/claudev/agenda/config/CorsConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @Configuration
 public class CorsConfig {
 
@@ -15,7 +17,8 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 WebMvcConfigurer.super.addCorsMappings(registry);
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200") // consenti solo Angular
+                        .allowedOrigins("http://localhost:4200") // consenti ad Angular
+                        .allowedOrigins("https://digital-agenda.it", "https://www.digital-agenda.it")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH")
                         .allowedHeaders("*")
 
